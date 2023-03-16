7ddd51e110d3d33537262a29b46902519de5f8c1
7417890522 <128030563+7417890522@users.noreply.github.com>
2023-03-16 14:47:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
